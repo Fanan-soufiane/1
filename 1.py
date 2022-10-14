87e8a40534cf0b37127e04d9b95354385c5cd39a
Fanan-soufiane <115195347+Fanan-soufiane@users.noreply.github.com>
2022-10-14 11:33:46 +0100

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,34 @@
+import os
+os.system ('pkg install cmatrix -y')
+os.system ('clear')
+os.system ('pkg install figlet -y')
+os.system ('pkg install sl -y')
+os.system ('clear')
+print ("\033[1;31m")
+os.system ('figlet Hk Free Fire')
+print ("    ERROR  ERROR  ERROR  ERROR  ERROR  ERROR  ERROR !!")
+print ("")
+print (' \033[1;32m{1}\033[1;33m Hacker Free Fire ID 0$')
+print ("")
+print (' \033[1;32m{2}\033[1;33m hacker instagram 10$ almost !')
+print ("")
+print (' \033[1;32m{3}\033[1;33m Hacker whtassap 0$ almost !')
+print ("")
+print (' \033[1;32m{4}\033[1;33m Hacker whatsapp Gb 0$ almost !')
+print ("")
+print ('  \033[1;32mFor Example, Choose a Number \033[1;31m1,2,')
+v7x2 = input (" \033[1;33mhacker\033[1;31m==> ")
+if v7x2 == '1' :
+        os.system ('1.sh')
+        os.system ('clear')
+        print ('')
+        print ("Loading..........!")
+        os.system ('figlet Hk Free Fiee ')
+fx9 = input (" \033[1;33mID Free Fire  ==> ")
+fx9 = input (" \033[1;35mDo you want to follow  yes = y no = n  ==> ")
+if fx9 == 'y' :
+        os.system ('python rot.py')
+if v7x2 == '2' :
+        os.system('xdg-open https://shortest.link/HLw')
+
+
